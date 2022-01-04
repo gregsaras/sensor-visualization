934544ed2363bb63f37335250f2e948571a6ece9
Greg S <gregorysaras@gmail.com>
2022-01-03 22:21:49 -0500

    Cleanup startup code

--- arduino/sensor-data/sensor-data.ino
+++ arduino/sensor-data/sensor-data.ino
@@ -7,16 +7,35 @@ void setup() {
   Serial.begin(9600);
   while(!Serial) { ; }
 
-  if(WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed");
-
-    // Don't continue if no WiFi module is detected
+  if(!has_WiFiModule) {
+    Serial.println("Communication with WiFi module failed, halting program");
     while(true);
+  } else {
+    Serial.println("Successfully detected WiFi module");
   }
+
+  printFirmwareInformation(); 
 }
 
 void loop() {
   Serial.println("Connecting to WiFi network: ");
   Serial.println(SSID);
   delay(10000);
+}
+
+boolean has_WiFiModule() {
+  return WiFi.status() != WL_NO_MODULE;
+}
+
+void printFirmwareInformation(){
+   String fv = WiFi.firmwareVersion();
+  if(fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Latest firmware version ");
+    Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
+  } else {
+    Serial.println("Firmware up to date");
+  }
+
+   Serial.println("Current Version: ");
+   Serial.println(fv);  
 }
