d387cca3e2b06356734b658637c439eb50900991
Greg S <gregorysaras@gmail.com>
2022-01-18 22:52:22 -0500

    Adding ping API end point

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import ping from './ping';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/ping', ping);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
---
+++ api/src/ping.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', (_, res) => {
+    res.send('pong');
+});
+
+export default router;
