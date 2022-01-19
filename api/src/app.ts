19f030942e3bb2d9be11b2efd2d909ca84ce7cc3
Greg S <gregorysaras@gmail.com>
2022-01-18 23:08:48 -0500

    Add a build command to package json

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,10 +4,6 @@ import ping from './ping';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.use('/ping', ping);
 
 app.listen(port, () => {
